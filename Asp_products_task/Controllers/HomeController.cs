ae1a7e39654a52fe98da99771628bbabd9cd6465
AhmadJabrayilov <tu25tyfpp@code.edu.az>
2022-05-24 03:28:52 +0400

    featured task

---
+++ Asp_products_task/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp_products_task.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Asp_products_task/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using Asp_products_task.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Asp_products_task.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index(string search)
+        {
+
+
+            List<Product> products = new List<Product>()
+            {
+
+                new Product
+                {
+                    Id = 1,
+                    Name = "Test 1",
+                    Description = "Test 1 description",
+                    Image = "product1.jpg",
+                    Price = 100
+                    
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Test 2",
+                    Description = "Test 2 description",
+                    Image = "product2.jfif",
+                    Price = 105
+
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Test 3",
+                    Description = "Test 3 description",
+                    Image = "product3.jfif",
+                    Price = 102
+
+                }
+
+            };
+
+            if(!string.IsNullOrWhiteSpace(search))
+            {
+              products =  products.FindAll(p => p.Name.ToLower().Contains(search.ToLower()));
+                if (products.Count == 0)
+                    return NotFound();
+            }
+
+            return View(products);
+        }
+    }
+}
